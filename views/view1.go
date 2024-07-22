02c79b46cf2641a6ea621f52f09c6aec23c84e84
Rewanth Tammana <22347290+rewanthtammana@users.noreply.github.com>
2024-07-22 21:44:10 +0400

    Update views/view1.go

--- views/view1.go
+++ views/view1.go
@@ -18,7 +18,7 @@ func View1(session neo4j.Session, params map[string]interface{}) {
 	ON CREATE SET summary.count = 1
 	ON MATCH SET summary.count = summary.count + 1
 	MERGE (e)-[:%s{view: 1}]->(summary)
-	MERGE (nsummary:%s_NAME_ {ip: $host_ip, entity: $entity, entity_type: $entity_type, name: $name, detection_type: $detection_type, view: 1})
+	MERGE (nsummary:%s_NAME {ip: $host_ip, entity: $entity, entity_type: $entity_type, name: $name, detection_type: $detection_type, view: 1})
 	ON CREATE SET nsummary.count = 1
 	ON MATCH SET nsummary.count = nsummary.count + 1
 	MERGE (summary)-[:%s{view: 1}]->(nsummary)`, params["detection_type"], params["detection_type"], params["name"], params["name"])
