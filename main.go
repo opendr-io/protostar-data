84038088ad6ca6d833791a16cf5b879f160b1e63
Rewanth Tammana <22347290+rewanthtammana@users.noreply.github.com>
2024-08-02 12:37:51 +0400

    Fix alerts in view 2 to include all types of detection types
    Signed-off-by: Rewanth Tammana <22347290+rewanthtammana@users.noreply.github.com>

--- main.go
+++ main.go
@@ -43,33 +43,34 @@ func insert(session neo4j.Session, filename string) {
 
 	gjson.Parse(modifiedJSON).ForEach(func(key, value gjson.Result) bool {
 		params := map[string]interface{}{
-			"source":       value.Get("source").String(),
-			"guid":         value.Get("guid").String(),
-			"timestamp":    value.Get("timestamp").String(),
-			"detection_type":  strings.ToUpper(reg.ReplaceAllString(value.Get("detection_type").String(), "_")),
-			"name":         strings.ToUpper(reg.ReplaceAllString(value.Get("name").String(), "_")),
-			"severity":     value.Get("severity").String(),
-			"category":     value.Get("category").String(),
-			"mitre_tactic": value.Get("mitre_tactic").String(),
-			"entity":       value.Get("entity").String(),
-			"entity_type":  value.Get("entity_type").String(),
-			"host_ip":      value.Get("host_ip").String(),
-			"source_ip":    value.Get("source_ip").String(),
-			"dest_ip":      value.Get("dest_ip").String(),
-			"dest_port":    value.Get("dest_port").String(),
-			"dst_geo":      value.Get("dst_geo").String(),
-			"username":     value.Get("username").String(),
-			"syscall_name": value.Get("syscall_name").String(),
-			"executable":   value.Get("executable").String(),
-			"process":      value.Get("process").String(),
-			"message":      value.Get("message").String(),
-			"proctitle":    value.Get("proctitle").String(),
+			"source":         value.Get("source").String(),
+			"guid":           value.Get("guid").String(),
+			"timestamp":      value.Get("timestamp").String(),
+			"detection_type": strings.ToUpper(reg.ReplaceAllString(value.Get("detection_type").String(), "_")),
+			"name":           strings.ToUpper(reg.ReplaceAllString(value.Get("name").String(), "_")),
+			"severity":       value.Get("severity").String(),
+			"category":       value.Get("category").String(),
+			"mitre_tactic":   value.Get("mitre_tactic").String(),
+			"entity":         value.Get("entity").String(),
+			"entity_type":    value.Get("entity_type").String(),
+			"host_ip":        value.Get("host_ip").String(),
+			"source_ip":      value.Get("source_ip").String(),
+			"dest_ip":        value.Get("dest_ip").String(),
+			"dest_port":      value.Get("dest_port").String(),
+			"dst_geo":        value.Get("dst_geo").String(),
+			"username":       value.Get("username").String(),
+			"syscall_name":   value.Get("syscall_name").String(),
+			"executable":     value.Get("executable").String(),
+			"process":        value.Get("process").String(),
+			"message":        value.Get("message").String(),
+			"proctitle":      value.Get("proctitle").String(),
 		}
 		views.View1(session, params)
 		views.View2(session, params)
 		bar.Add(1)
 		return true
 	})
+	fmt.Println()
 }
 
 func main() {
@@ -99,6 +100,10 @@ func main() {
 	insert(session, "data/inputs.json")
 	insert(session, "data/40k.json")
 	insert(session, "data/ct.json")
+	insert(session, "data/hot-3.json")
+	insert(session, "data/cloudtrail-3k.json")
+	insert(session, "data/isotopes-4.json")
+	insert(session, "data/linux.json")
 
 	fmt.Println("Data imported successfully.")
 }
