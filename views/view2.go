306b5c65c7f8af694955ea72d94bc8d2c4136f87
Rewanth Tammana <22347290+rewanthtammana@users.noreply.github.com>
2024-08-03 14:40:17 +0400

    Refine severity cluster data
    Signed-off-by: Rewanth Tammana <22347290+rewanthtammana@users.noreply.github.com>

--- views/view2.go
+++ views/view2.go
@@ -16,56 +16,33 @@ func View2(session neo4j.Session, params map[string]interface{}) {
 	res, err := session.Run(query, params)
 	utils.HandleResult(res, err)
 
-	// fmt.Println("Creating unique set of entities 3")
 	query = `
+	// Match the ENTITY node with the given parameters
 	MATCH (e:ENTITY {ip: $host_ip, entity: $entity, entity_type: $entity_type, view: 2})
-	MERGE (ad:AS_DEST {ip: $host_ip, entity: $entity, entity_type: $entity_type, view: 2, dest_ip: $dest_ip})
-	ON CREATE SET ad.count = 1
-	ON MATCH SET ad.count = ad.count + 1
-	WITH e, ad
-	MATCH (e_dest:ENTITY {ip: $dest_ip, view: 2})
-	MERGE (e_dest)-[:AS_DEST]->(ad)
-	
-	MERGE (sc:SEVERITY_CLUSTER {ip: $host_ip, entity: $entity, entity_type: $entity_type, view: 2, dest_ip: $dest_ip, severity: $severity})
-	ON CREATE SET sc.count = 1
-	ON MATCH SET sc.count = sc.count + 1
-	MERGE (ad)-[:HAS_SEVERITY]->(sc)
-	
-	MERGE (nc:NAME_CLUSTER {ip: $host_ip, entity: $entity, entity_type: $entity_type, view: 2, dest_ip: $dest_ip, severity: $severity, name: $name})
-	ON CREATE SET nc.count = 1
-	ON MATCH SET nc.count = nc.count + 1
-	MERGE (sc)-[:NAME_CLUSTER]->(nc)
-	
-	CREATE (a:ALERT {guid: $guid, timestamp: $timestamp, detection_type: $detection_type, name: $name, category: $category, mitre_tactic: $mitre_tactic, entity: $entity, entity_type: $entity_type, host_ip: $host_ip, source_ip: $source_ip, dest_ip: $dest_ip, dest_port: $dest_port, dst_geo: $dst_geo, username: $username, syscall_name: $syscall_name, executable: $executable, process: $process, message: $message, proctitle: $proctitle, severity: $severity, view: 2})
-	MERGE (nc)-[:INCLUDES]->(a)
-	`
-	res, err = session.Run(query, params)
-	utils.HandleResult(res, err)
 
-	// fmt.Println("Creating unique set of entities 4")
-	query = `
-	MATCH (e:ENTITY {ip: $host_ip, entity: $entity, entity_type: $entity_type, view: 2})
-	MERGE (as:AS_SOURCE {ip: $host_ip, entity: $entity, entity_type: $entity_type, view: 2, source_ip: $source_ip})
-	ON CREATE SET as.count = 1
-	ON MATCH SET as.count = as.count + 1
-	WITH e, as
-	MATCH (e_source:ENTITY {ip: $source_ip, view: 2})
-	MERGE (e_source)-[:AS_SOURCE]->(as)
-	
-	
-	MERGE (scs:SEVERITY_CLUSTER {ip: $host_ip, entity: $entity, entity_type: $entity_type, view: 2, source_ip: $source_ip, severity: $severity})
+	// Merge the SEVERITY_CLUSTER node and update its count
+	MERGE (scs:SEVERITY_CLUSTER {ip: $host_ip, entity: $entity, entity_type: $entity_type, view: 2, severity: $severity})
 	ON CREATE SET scs.count = 1
 	ON MATCH SET scs.count = scs.count + 1
-	MERGE (as)-[:HAS_SEVERITY]->(scs)
-	
-	MERGE (ncs:NAME_CLUSTER {ip: $host_ip, entity: $entity, entity_type: $entity_type, view: 2, source_ip: $source_ip, severity: $severity, name: $name})
+
+	// Create the relationship between ENTITY and SEVERITY_CLUSTER
+	MERGE (e)-[:HAS_SEVERITY]->(scs)
+
+	// Merge the NAME_CLUSTER node and update its count
+	MERGE (ncs:NAME_CLUSTER {ip: $host_ip, entity: $entity, entity_type: $entity_type, view: 2, severity: $severity, name: $name})
 	ON CREATE SET ncs.count = 1
 	ON MATCH SET ncs.count = ncs.count + 1
+
+	// Create the relationship between SEVERITY_CLUSTER and NAME_CLUSTER
 	MERGE (scs)-[:NAME_CLUSTER]->(ncs)
-	
+
+	// Create the ALERT node with the given parameters
 	CREATE (aa:ALERT {guid: $guid, timestamp: $timestamp, detection_type: $detection_type, name: $name, category: $category, mitre_tactic: $mitre_tactic, entity: $entity, entity_type: $entity_type, host_ip: $host_ip, source_ip: $source_ip, dest_ip: $dest_ip, dest_port: $dest_port, dst_geo: $dst_geo, username: $username, syscall_name: $syscall_name, executable: $executable, process: $process, message: $message, proctitle: $proctitle, severity: $severity, view: 2})
+
+	// Create the relationship between NAME_CLUSTER and ALERT
 	MERGE (ncs)-[:INCLUDES]->(aa)
 	`
+
 	res, err = session.Run(query, params)
 	utils.HandleResult(res, err)
 }
